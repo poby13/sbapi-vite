10a1d8569a3b94635175d01ef3d43e38243b13e4
shinis(home) <actionshin@gmail.com>
2024-11-28 00:59:53 +0900

    스프링 부트 3.3.6 프로젝트 구성

---
+++ src/main/java/kr/co/cofile/sbapivite/SbapiViteApplication.java
@@ -0,0 +1,13 @@
+package kr.co.cofile.sbapivite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbapiViteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbapiViteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/cofile/sbapivite/SbapiViteApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.cofile.sbapivite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbapiViteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
