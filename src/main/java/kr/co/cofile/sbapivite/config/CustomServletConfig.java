c362b56da113d7c9bd2ae9a91d6ca4c0629e1662
shinis(ys) <actionshin@gmail.com>
2024-12-02 15:50:59 +0900

    CORS(교차 출력 리소스 공유) 제한 관리

--- src/main/java/kr/co/cofile/sbapivite/config/CustomServletConfig.java
+++ src/main/java/kr/co/cofile/sbapivite/config/CustomServletConfig.java
@@ -3,6 +3,7 @@ package kr.co.cofile.sbapivite.config;
 import kr.co.cofile.sbapivite.controller.formatter.LocalDateFormater;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,5 +12,14 @@ public class CustomServletConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addFormatter(new LocalDateFormater());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .maxAge(300)
+                .allowedHeaders("Authorization", "Cache-Control", "Content-Type");
     }
 }
